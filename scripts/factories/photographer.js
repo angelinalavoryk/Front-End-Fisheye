fdc91e7818abafd606e85c5cb04afd361f762371
Angelina <50414739+angelinalavoryk@users.noreply.github.com>
2023-03-12 12:53:03 +0100

    les trois premières étapes du projet: récupérés les données, créer des cartes pour les photographes, afficher les données

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,17 +1,52 @@
-function photographerFactory(data) {
-    const { name, portrait } = data;
+  //Fonction 'photographerFactory' crée pour chaque photographes la photo, le nom et la carte d'informations (avec la fonction 'getUserCardDOM')
+  function photographerFactory(data) {
+    const { name, portrait, city, country, id, price, tagline } = data;//(destructuration) extrait les infos et retourne l'objet avec ces infos 
 
-    const picture = `assets/photographers/${portrait}`;
+    const picture = `assets/photographers/${portrait}`;//chemin vers les photos dans dossier 'assets'
 
     function getUserCardDOM() {
-        const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
-        h2.textContent = name;
-        article.appendChild(img);
-        article.appendChild(h2);
-        return (article);
+      const photographerCardElement = document.createElement('article');//créé <article> (le container) pour le contenu de chaque photographes. 
+    
+
+      const photographerLinkElement = document.createElement('a');//lien vers la page 'photographer.html' pour chaque photographe.
+      photographerLinkElement.href = `photographer.html?id=${id}`;
+      photographerCardElement.appendChild(photographerLinkElement);//<a> enfant de <article>
+  
+  
+  
+      const photographerImageElement = document.createElement('img');//photo du photographe
+      photographerImageElement.src = `assets/photographers/${portrait}`;//emplacement des photos.
+      photographerImageElement.alt = name;//alt pour la description de la photo.
+      photographerLinkElement.appendChild(photographerImageElement);//img enfant de <a>
+  
+  
+      const photographerNameElement = document.createElement('h2');//Nom du photographe
+      photographerNameElement.textContent = name;
+      photographerLinkElement.appendChild(photographerNameElement);//h2 nom est l'enfant de <a>
+  
+      const photographerInfoElement = document.createElement('article');//deuxiemem <article> pour les infos en dessous de la photo des photographe
+      photographerInfoElement.classList.add('info');
+      photographerCardElement.appendChild(photographerInfoElement);//<article 2> enfant du premier <article>
+  
+  
+      const photographerLocationElement = document.createElement('h3');//Ville
+      photographerLocationElement.classList.add('location');
+      photographerLocationElement.textContent = `${city}, ${country}`;
+      photographerInfoElement.appendChild(photographerLocationElement);//enfant de l'article 2
+  
+  
+      const photographerTaglineElement = document.createElement('p');//phrase
+      photographerTaglineElement.classList.add('tagline');
+      photographerTaglineElement.textContent = tagline;
+      photographerInfoElement.appendChild(photographerTaglineElement);//enfnat de l'article 2
+  
+      
+      const photographerPriceElement = document.createElement('p');//prix
+      photographerPriceElement.classList.add('price');
+      photographerPriceElement.textContent = `${price}€ / jour`;
+      photographerInfoElement.appendChild(photographerPriceElement);//enfant de l'article 2
+
+      return photographerCardElement; //renvoie l'artcile avec les infos
     }
-    return { name, picture, getUserCardDOM }
+  return { name, picture, getUserCardDOM }//renvoie l'objet name et img en appelant la fonction 'getUserCardDOM' qui est dans la fonction 'photographerFactory'
 }
--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,46 +1,27 @@
-    async function getPhotographers() {
-        // Ceci est un exemple de données pour avoir un affichage de photographes de test dès le démarrage du projet, 
-        // mais il sera à remplacer avec une requête sur le fichier JSON en utilisant "fetch".
-        let photographers = [
-            {
-                "name": "Ma data test",
-                "id": 1,
-                "city": "Paris",
-                "country": "France",
-                "tagline": "Ceci est ma data test",
-                "price": 400,
-                "portrait": "account.png"
-            },
-            {
-                "name": "Autre data test",
-                "id": 2,
-                "city": "Londres",
-                "country": "UK",
-                "tagline": "Ceci est ma data test 2",
-                "price": 500,
-                "portrait": "account.png"
-            },
-        ]
-        // et bien retourner le tableau photographers seulement une fois récupéré
-        return ({
-            photographers: [...photographers, ...photographers, ...photographers]})
-    }
+//La fonction 'getPhotographers' récupère la liste des photographes à partir du fichier json en utilisant fetch
+async function getPhotographers() {
+    const response = await fetch('./data/photographers.json');//Fetch() récuperère les données à partir du fichier json qui est dans le dossier 'data'. 
+    const data = await response.json();//convertir les données json en objet 
+    const {photographers} = data;//extraire la liste des photographes à partir de l'objet 
+    return photographers;//renvoie la liste des photographes
+}
 
-    async function displayData(photographers) {
-        const photographersSection = document.querySelector(".photographer_section");
 
-        photographers.forEach((photographer) => {
-            const photographerModel = photographerFactory(photographer);
-            const userCardDOM = photographerModel.getUserCardDOM();
-            photographersSection.appendChild(userCardDOM);
-        });
-    };
+//Fonction 'displayData' affiche les cartes d'infos des photographes. 
+async function displayData(photographers) {//la fonction prends en paramètre la liste de photographes. 
+    const photographersSection = document.querySelector(".photographer_section");//récupère la div html
 
-    async function init() {
-        // Récupère les datas des photographes
-        const { photographers } = await getPhotographers();
-        displayData(photographers);
-    };
-    
-    init();
-    
+    photographers.forEach((photographer) => {//boucle forEach parcour chaque photographe de la liste.
+        const photographerModel = photographerFactory(photographer);//pour chaque photographe elle appelle la fonction 'photographerFactory'
+        const userCardDOM = photographerModel.getUserCardDOM();//et crée une carte d'info pour les photographes
+        photographersSection.appendChild(userCardDOM);//ajoute les cartes 'userCardDOM' comme des enfants de la div = photographer_section.
+    });
+};
+
+
+//Fonction 'init' permet d'initialiser l'affichage de la liste des photographes
+async function init() {
+    const photographers = await getPhotographers();//appelle la fonction 'getPhotographers' pour récupérer la liste des photographes.
+    displayData(photographers);//appelle la fonction 'displayData' pour afficher les cartes de chaque photographe.
+};
+init();
